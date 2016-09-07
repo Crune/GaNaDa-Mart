aa92bef7e268e5e0f3da0248b8faa1e72933a935
Rune Choe <rune.choe@gmail.com>
2016-09-07 19:08:30 +0900

    템플릿 적용

--- WebContent/jsp/menu/main.jsp
+++ WebContent/jsp/menu/main.jsp
@@ -1,5 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
-<jsp:include page="/jsp/template/header.jsp" flush="true" />
-메인페이지 입니다.
-<jsp:include page="/jsp/template/footer.jsp" flush="true" />
+<jsp:include page="${urlHeader}" flush="false" />
+<jsp:include page="${urlImg}" flush="false" />
+<jsp:include page="${urlSub}" flush="false" />
+<jsp:include page='${urlFooter}' flush="false" />
--- src/ganada/action/MainAction.java
+++ src/ganada/action/MainAction.java
@@ -2,14 +2,28 @@ package ganada.action;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class MainAction implements SuperAction {
 
-	@Override
-	public String executeAction(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		// TODO Auto-generated method stub
-	    
-		return "/jsp/menu/main.jsp";
-	}
+    private static MainImageAction imgAction = new MainImageAction();
+    private static SubModuleAction subAction = new SubModuleAction();
+
+    @Override
+    public String executeAction(HttpServletRequest request, HttpServletResponse response) throws Exception {
+	// TODO Auto-generated method stub
+
+	HttpSession session = request.getSession();
+	session.setAttribute("pageTitle", "메인화면 입니다");
+	session.setAttribute("menu", "0");
+	
+	String url = "";
+	url = imgAction.executeAction(request, response);
+	session.setAttribute("urlImg", (String)url);
+	url = subAction.executeAction(request, response);
+	session.setAttribute("urlSub", (String)url);
+
+	return "/jsp/menu/main.jsp";
+    }
 
 }
