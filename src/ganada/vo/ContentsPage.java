a78025687248e1b9a586136c790e3a1ea221a9ed
Crune <rune.choe@gmail.com>
2016-09-07 21:12:40 +0900

    vo 생성

---
+++ src/ganada/vo/ContentsPage.java
@@ -0,0 +1,14 @@
+package ganada.vo;
+
+import java.util.List;
+
+public class ContentsPage {
+
+    private String code = "";
+    private String name = "";
+    private String description = "";
+
+    private String mainImageFileName = "";
+    private List<SubModule> subList;
+    
+}
