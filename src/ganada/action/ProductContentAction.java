e559c66e7a0fdd72f7c5a6c6dc21d5366208b1bd
seongsu <ssscpm@gmail.com>
2016-09-13 16:06:20 +0900

    product Action추가, properties경로추가

---
+++ src/ganada/action/ProductContentAction.java
@@ -0,0 +1,12 @@
+package ganada.action;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ProductContentAction implements SuperAction{
+	public String executeAction(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		
+		
+		return "/jsp/product/productContent.jsp";
+	}
+}
