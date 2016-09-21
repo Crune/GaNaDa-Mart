9a6d0e32e0c554c018d13c7c2cd7ec8187d0acdb
seongsu <ssscpm@gmail.com>
2016-09-21 21:12:12 +0900

    상품데이터베이스ㅅ수우우우우정

---
+++ src/ganada/obj/product/Color.java
@@ -0,0 +1,26 @@
+package ganada.obj.product;
+
+public class Color {
+	private String c_code;				//색상코드
+	private String im_code;				//이미지코드
+	private String c_name;				//색상이름
+	
+	public String getC_code() {
+		return c_code;
+	}
+	public void setC_code(String c_code) {
+		this.c_code = c_code;
+	}
+	public String getIm_code() {
+		return im_code;
+	}
+	public void setIm_code(String im_code) {
+		this.im_code = im_code;
+	}
+	public String getC_name() {
+		return c_name;
+	}
+	public void setC_name(String c_name) {
+		this.c_name = c_name;
+	}
+}
---
+++ src/ganada/obj/product/Stock.java
@@ -0,0 +1,47 @@
+package ganada.obj.product;
+
+public class Stock {
+	private String sk_code;				//재고코드
+	private String st_code;				//창고코드
+	private String c_code;				//색상코드
+	private String pd_size;				//사이즈
+	private int amount;				//재고량
+	private String pd_code;				//상품코드
+	
+	public String getSk_code() {
+		return sk_code;
+	}
+	public void setSk_code(String sk_code) {
+		this.sk_code = sk_code;
+	}
+	public String getSt_code() {
+		return st_code;
+	}
+	public void setSt_code(String st_code) {
+		this.st_code = st_code;
+	}
+	public String getC_code() {
+		return c_code;
+	}
+	public void setC_code(String c_code) {
+		this.c_code = c_code;
+	}
+	public String getPd_size() {
+		return pd_size;
+	}
+	public void setPd_size(String pd_size) {
+		this.pd_size = pd_size;
+	}
+	public int getAmount() {
+		return amount;
+	}
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+	public String getPd_code() {
+		return pd_code;
+	}
+	public void setPd_code(String pd_code) {
+		this.pd_code = pd_code;
+	}
+}
