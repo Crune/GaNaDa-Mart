2055cea34d31b9f9f900b6b5125e33c2bd1bed42
seongsu <ssscpm@gmail.com>
2016-09-28 16:38:53 +0900

    재고 수정하는거 작업중

---
+++ src/ganada/mc/action/StockUpdateAction.java
@@ -0,0 +1,19 @@
+package ganada.mc.action;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import ganada.action.common.SuperAction;
+
+public class StockUpdateAction implements SuperAction {
+
+	public String executeAction(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		String pd_code = request.getParameter("pd_code");
+		
+		
+		request.setAttribute("pd_code", pd_code);
+		
+		return "/jsp/mc/pmc/stockUpdate.jsp";
+	}
+
+}
