9728af17cf5b651e6ab9813e1842b9190840e506
seongsu <ssscpm@gmail.com>
2016-09-28 21:06:16 +0900

    상품등록 정보등록과 연동중

--- WebContent/jsp/mc/pmc/insert.jsp
+++ WebContent/jsp/mc/pmc/insert.jsp
@@ -1,7 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
-
-
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
     
     <input type="button" value="목록으로" onclick="javascript:window.location='./list.gnd'">
 <input type="button" value="관리자 메인" onclick="javascript:window.location='./index.gnd'">
@@ -22,6 +21,14 @@
     <input id="pd_infocode3" name="pd_infocode3" type="text" size="50" placeholder="상품명" maxlength="50">
     <li><label for="pd_infocode4">정보코드4</label>
     <input id="pd_infocode4" name="pd_infocode4" type="text" size="50" placeholder="상품명" maxlength="50">
+   
+    <li><label for="pd_infocode">정보</label>
+    	<select name="pd_infotype">
+			<option value="" selected>정보구분</option>
+			<forEach var="i" items="${piList }">
+				<option value="${i }">${piList.pd_infocontent}</option>
+			</forEach>
+		</select>
     
     <li class="label2">
 
--- src/ganada/mc/action/InsertAction.java
+++ src/ganada/mc/action/InsertAction.java
@@ -1,15 +1,30 @@
 package ganada.mc.action;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import ganada.action.common.SuperAction;
+import ganada.obj.product.ProductInfo;
+import ganada.obj.product.ProductInfoDao;
 
 public class InsertAction implements SuperAction {
 
 	
 	public String executeAction(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		request.setAttribute("type", new Integer(0));
+		//String pd_infocode = request.getParameter("pd_infocode");
+		
+		ProductInfoDao dbPro = ProductInfoDao.getInstance();
+		//ProductInfo pi = (ProductInfo) dbPro.select(pd_infocode);
+		
+		int count = dbPro.count();
+		String startCode = "1";
+		String endCode = String.valueOf(count);		
+		List<Object> piList = dbPro.select(startCode, endCode);
+		//request.setAttribute("pi", pi);
+		request.setAttribute("piList", piList);
+		//request.setAttribute("type", new Integer(0));
 		return "/jsp/mc/pmc/insert.jsp";
 	}
 
