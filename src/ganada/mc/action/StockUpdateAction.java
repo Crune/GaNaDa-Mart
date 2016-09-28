a002919a3a923fc9b9a1f07735aa9edad8cb1490
seongsu <ssscpm@gmail.com>
2016-09-28 17:06:09 +0900

    .

--- src/ganada/mc/action/StockUpdateAction.java
+++ src/ganada/mc/action/StockUpdateAction.java
@@ -4,14 +4,19 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import ganada.action.common.SuperAction;
+import ganada.obj.product.Stock;
+import ganada.obj.product.StockDao;
 
 public class StockUpdateAction implements SuperAction {
 
 	public String executeAction(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		String pd_code = request.getParameter("pd_code");
+		String sk_code = request.getParameter("sk_code");
 		
+		StockDao dbPro = StockDao.getInstance();
+		Stock stock = (Stock) dbPro.select(sk_code);
 		
-		request.setAttribute("pd_code", pd_code);
+		request.setAttribute("stock", stock);
+		request.setAttribute("sk_code", sk_code);
 		
 		return "/jsp/mc/pmc/stockUpdate.jsp";
 	}
