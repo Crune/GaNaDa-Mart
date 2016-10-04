4f15f7c9b7dab12d0201a092420fd05b568c13e1
seongsu <ssscpm@gmail.com>
2016-10-04 21:04:22 +0900

    .

--- WebContent/jsp/mc/pmc/imageInsert.jsp
+++ WebContent/jsp/mc/pmc/imageInsert.jsp
@@ -1,3 +1,32 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+
+<form id="imgup" action="./imageInsertPro.gnd?pd_code=${Image.getPd_code() }" method="post" encType="multipart/form-data">
+<input type="hidden" name="im_order" value="1">
+right : <input type="file" name="right"> 
+<select id="c_code" name="c_code">
+<option value="1">1</option> 
+<option value="2">2</option>
+<option value="3">3</option>
+<option value="4">4</option>
+<option value="5">5</option>
+</select>	<br />
+<input type="hidden" name="im_order" value="2">
+left : <input type="file" name="left"> <br />
+
+<input type="hidden" name="im_order" value="3">
+bot : <input type="file" name="bot"><br />
+
+<input type="hidden" name="im_order" value="4">
+top : <input type="file" name="top"><br />
+
+<input type="hidden" name="im_order" value="5">
+image : <input type="file" name="image"><br />
+
+<input type="hidden" name="im_order" value="6">
+back : <input type="file" name="back"><br />
+
+<input type="submit"  value="파일올리기">
+
+</form>
--- src/ganada/mc/action/ImageInsertProAction.java
+++ src/ganada/mc/action/ImageInsertProAction.java
@@ -1,13 +1,58 @@
 package ganada.mc.action;
 
+import java.util.Enumeration;
+
+import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.oreilly.servlet.MultipartRequest;
+import com.oreilly.servlet.multipart.DefaultFileRenamePolicy;
+
 import ganada.action.common.SuperAction;
+import ganada.obj.product.ItemReview;
+import ganada.obj.product.ItemReviewDao;
+import ganada.obj.product.ProductImage;
+import ganada.obj.product.ProductImageDao;
 
 public class ImageInsertProAction implements SuperAction {
 	public String executeAction(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		String filename="";
+		String realFolder="";
+		String saveFolder="/img";
+		//String path = request.getRealPath("img");
+		int maxSize = 1024*1024*10; 
+		String enc = "UTF-8";
+		DefaultFileRenamePolicy dp = new DefaultFileRenamePolicy();
+		String im_name="";
+		ServletContext context = request.getSession().getServletContext();
+		realFolder = context.getRealPath(saveFolder);
+		MultipartRequest mr = new MultipartRequest(request,realFolder,maxSize,enc,dp);
+		//String im_name = mr.getFilesystemName("product_image");
+		Enumeration params = mr.getParameterNames();
+		while(params.hasMoreElements()){
+		filename = (String)params.nextElement();
+		String orgname  = mr.getOriginalFileName(filename);
+		im_name=mr.getFilesystemName("filename");
+		
+		}
+		ProductImage Image = new ProductImage();
+		//String im_code = mr.getParameter("im_code");
+		String pd_code = mr.getParameter("pd_code");
+		String im_order = mr.getParameter("im_order");
+		String pd_name = mr.getParameter("pd_name");
+		
 
+		
+		Image.setIm_name(im_name);
+        System.out.println(Image.toString());
+		ProductImageDao  dao = ProductImageDao.getInstance();
+		dao.insert(Image);
+		
+		
+		request.setAttribute("pd_code", pd_code);
+		request.setAttribute("im_name", im_name);
+		
 	
 	
 	return "/jsp/mc/pmc/imageInsertPro.jsp";
