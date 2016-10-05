b6238a339daf3030be495bf50ec5159906eb3208
seongsu <ssscpm@gmail.com>
2016-10-05 20:51:19 +0900

    재고등록 수정 삭제추가

--- WebContent/jsp/mc/pmc/stockInsertPro.jsp
+++ WebContent/jsp/mc/pmc/stockInsertPro.jsp
@@ -1,4 +1,4 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
-<c:redirect url="./list.gnd" />
+<c:redirect url="./mc.gnd?menu=stock&pd_code=${pd_code }" />
--- src/ganada/mc/action/StockDeleteProAction.java
+++ src/ganada/mc/action/StockDeleteProAction.java
@@ -10,9 +10,12 @@ public class StockDeleteProAction implements SuperAction {
 	public String executeAction(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		request.setCharacterEncoding("UTF-8");
 		String sk_code = request.getParameter("sk_code");
+		String pd_code = request.getParameter("pd_code");
 		
 		StockDao dbPro = StockDao.getInstance();
 		dbPro.delete(sk_code);
+		
+		request.setAttribute("pd_code", pd_code);
 		
 		return "/jsp/mc/pmc/stockDeletePro.jsp";
 	}
